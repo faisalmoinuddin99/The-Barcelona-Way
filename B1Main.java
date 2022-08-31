aeb4bd1cfa4c11df7b45d15f06385623d9391ba2
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-31 10:28:17 +0530

    User Input/Output

---
+++ B1Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class B1Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // taking integer as an input
+        int a = sc.nextInt();
+        System.out.println(a);
+
+        // taking float as an input
+        float b = sc.nextFloat() ;
+        System.out.println(b);
+
+        // taking string as an input
+        String name = sc.next() ;
+        System.out.println(name);
+
+    }
+}
