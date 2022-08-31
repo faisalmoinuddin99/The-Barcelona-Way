199cbf155f590050f2f2115d1e6476fbe319d63d
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-31 11:41:48 +0530

    Odd Number Pyramid Shape Pattern Program

--- Main.java
+++ Main.java
@@ -1,22 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-       int n = 5 ;
+        int i,j,k=5, p= 0 ;
 
-       for(int i = n; i>= 1; i--){
-           for (int j = 1; j <=i ; j++){
-               System.out.print(j);
-           }
-           System.out.println();
-       }
+        for(i = 1; i <= 5; i++){
+            for(j = 1; j < k; j++){
+                System.out.print(" ");
+            }
+            k-- ;
+            for (int s = 0 ; s < i+p ;s++){
+                System.out.print(2 * s + 1);
+            }
+            System.out.println();
+            p++ ;
+        }
+
     }
 }
 /*
 OUTPUT:
 
-    12345
-    1234
-    123
-    12
     1
+   135
+  13579
+ 135791113
+1357911131517
 
  */
