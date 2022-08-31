91e556ff395b6d31e0678622be5538e579454d82
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-31 13:28:37 +0530

    Reverse a Number

--- Main.java
+++ Main.java
@@ -2,26 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int N = 1234575565 ;
+        int n = 123 ;
+        int num = n ;
+        int reverse = 0 ;
 
-        int count = 0 ;
-
-        // APPROACH 1 - Tn: O(n)
-        while(N != 0){
-            N = N / 10 ;
-            count++ ;
+        while(n != 0){
+            int digit = n % 10 ;
+            reverse = reverse * 10 + digit ;
+            n = n / 10 ;
         }
-        System.out.println(count);
-
-
-        // APPROACH 2 - Tn: O(1)
-        int newN = 1234575565 ;
-        String convertedN = String.valueOf(newN) ;
-        System.out.println(convertedN.length());
-
-        // APPROACH 3 - Tn: O(1)
-        int approachThirdNumber = 123456 ;
-        int digit = (int) Math.floor(Math.log10(approachThirdNumber) + 1);
-        System.out.println(digit);
+        System.out.println(num + " " +reverse); // 123 321
     }
 }
