655d07891be2a09de4649e59d086312f943a6e0b
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-31 14:00:45 +0530

    Check Number is Palindrom or Not

--- Main.java
+++ Main.java
@@ -2,15 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int n = 123 ;
-        int num = n ;
-        int reverse = 0 ;
+        int N = -5555 ;
+        String result = isPalindrome(N) ;
+        System.out.println(result);
+    }
 
-        while(n != 0){
-            int digit = n % 10 ;
+    public static boolean isPositive(int number){
+        return number >= 0;
+    }
+    public static String isPalindrome(int number){
+       if (isPositive(number)) {
+        int copiedNumber = number ;
+        int reverse = 0 ;
+        while(copiedNumber != 0){
+            int digit = copiedNumber % 10 ;
             reverse = reverse * 10 + digit ;
-            n = n / 10 ;
+            copiedNumber = copiedNumber / 10 ;
         }
-        System.out.println(num + " " +reverse); // 123 321
+        System.out.println(reverse);
+        if (reverse == number) {
+            return "Yes" ;
+        }else {
+            return "No" ;
+        }
+       }
+       return "No" ;
     }
 }
