7e5f970b0050635ec3a12b3500aa94c055400669
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-31 10:53:07 +0530

    Done with 5th Pattern Problem

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
        int n = 5 ;
 
        for (int i = 1; i <= n; i++){
-           for (int j = 1; j <=i ; j++){
+           for (int j = i; j <=n ; j++){
                System.out.print("* ");
            }
            System.out.println();
@@ -13,10 +13,10 @@ public class Main {
 /*
 OUTPUT:
 
-       *
-       * *
-       * * *
-       * * * *
-       * * * * *
+   * * * * *
+   * * * *
+   * * *
+   * *
+   *
 
  */
