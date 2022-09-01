ad5bd906df6815fb439b38e686345c5d16215bf9
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-09-01 15:47:28 +0530

    Armstrong Number or Not

--- Main.java
+++ Main.java
@@ -2,30 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int N = -5555 ;
-        String result = isPalindrome(N) ;
-        System.out.println(result);
-    }
+      int N = 153 ;
+      String armstrongNumberOrNot = armstrongNumber(N) ;
+        System.out.println(armstrongNumberOrNot);
+      }
 
-    public static boolean isPositive(int number){
-        return number >= 0;
-    }
-    public static String isPalindrome(int number){
-       if (isPositive(number)) {
-        int copiedNumber = number ;
-        int reverse = 0 ;
-        while(copiedNumber != 0){
-            int digit = copiedNumber % 10 ;
-            reverse = reverse * 10 + digit ;
-            copiedNumber = copiedNumber / 10 ;
+    public static String armstrongNumber(int N) {
+        int cloneOfN = N ;
+        int sum = 0 ;
+        while (N != 0){
+            int digit = N % 10 ;
+            sum = (int) (sum + Math.pow(digit,3));
+            N = N / 10;
         }
-        System.out.println(reverse);
-        if (reverse == number) {
+
+        if (sum == cloneOfN){
             return "Yes" ;
         }else {
             return "No" ;
         }
-       }
-       return "No" ;
     }
 }
+
+
