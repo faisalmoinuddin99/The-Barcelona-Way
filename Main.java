510ead6002d976ea9a9d21a3a4bb4ffac2d70506
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-09-03 00:42:35 +0530

    Check for prime

--- Main.java
+++ Main.java
@@ -2,25 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // optimized code for LCM and GCD
-        Scanner sc = new Scanner(System.in) ;
-        Long N1 = sc.nextLong();
-        Long N2 = sc.nextLong();
+        int N = 6;
+        boolean ans = isPrime(N);
+//        System.out.println(ans );
 
-    }
-
-    static Long gcd(Long A, Long B){
-        if(B == 0){
-            return A ;
+        if (ans) {
+            System.out.println("yes");
+        } else {
+            System.out.println("No");
         }
-        return gcd(B, A % B) ;
     }
-    static Long[] gcdAndLcm(Long A, Long B){
-        Long gcdANS = gcd(A, B) ;
-        Long lcm = ((A * B) / gcdANS) ;
 
-        return new Long[]{lcm, gcdANS};
+    static boolean isPrime(int N) {
+        for (int i = 2; i < N; i++) {
+            if (N % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
+
 }
 
 
