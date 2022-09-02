643478408be4b559d03088668e66038596d2a2ba
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-09-03 02:10:14 +0530

    PrintNumber using recusion

--- Main.java
+++ Main.java
@@ -1,36 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-
-    static int counter = 0 ;
     public static void main(String[] args) {
-        //f() ; // function call
-        printName(5) ;
+        // Print numbers from 1 to N without the help of loops.
+        printNumber(10);
     }
-    static void f(){
+   static  public void  printNumber(int N){
 
-        // Base condition
-        if(counter == 3){
-            return ; // backtrack
-        }
-        System.out.print(counter + " ");
-        counter++ ;
-        f() ; // recursive call
-    }
-   static void printName(int N){
-        if(counter == N){
-            return ;
-        }
-        System.out.println("faisal " + counter);
-        counter++ ;
-        printName(N);
+       if(N == 0) return;
+       printNumber(N - 1);
+       System.out.print(N + " ");
     }
 }
-/* OUTPUT:
-        faisal 0
-        faisal 1
-        faisal 2
-        faisal 3
-        faisal 4
- */
 
