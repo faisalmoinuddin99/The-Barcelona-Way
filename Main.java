2f11c6ad00f5c91dc30fa0640e4d8fffc38cb59f
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-09-02 21:23:21 +0530

    Optimized code for LCM and GCD

--- Main.java
+++ Main.java
@@ -2,38 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int N1 = 14;
-        int N2 = 8;
-        int approach1 = findGCDApproach1(N1, N2);
-        int approach2 = gcd(N1, N2) ;
-//        System.out.println(approach1);
-        System.out.println("GCD: "+approach2);
+        // optimized code for LCM and GCD
+        Scanner sc = new Scanner(System.in) ;
+        Long N1 = sc.nextLong();
+        Long N2 = sc.nextLong();
 
-        int lcm = findLCM(N1, N2) ;
-        System.out.println("LCM: "+ lcm);
     }
 
-    // Tn: O(n)
-    public static int findGCDApproach1(int num1, int num2) {
-        int min = Math.min(num1, num2);
-        int ans = 0;
-        for (int i = 1; i <= min; i++) {
-            if (num1 % i == 0 && num2 % i == 0) {
-                ans = i;
-            }
+    static Long gcd(Long A, Long B){
+        if(B == 0){
+            return A ;
         }
-        return ans;
+        return gcd(B, A % B) ;
     }
-    public static int findLCM(int num1, int num2){
-        int gcd = findGCDApproach1(num1,num2) ;
-        return (num1 * num2) / gcd;
-    }
-    // Tn: O(log_phi min(a,b)) where phi = 1.61
-    public static int gcd(int num1, int num2) {
-        if (num2 == 0){
-            return num1 ;
-        }
-        return gcd(num2,num1 % num2) ;
+    static Long[] gcdAndLcm(Long A, Long B){
+        Long gcdANS = gcd(A, B) ;
+        Long lcm = ((A * B) / gcdANS) ;
+
+        return new Long[]{lcm, gcdANS};
     }
 }
 
